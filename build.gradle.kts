707983838a300dca1aa85d176cb8340baaddbadd
Tamas Balog <picimako@protonmail.com>
2025-06-29 10:57:39 +0200

    Update some dependencies and project configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,6 @@ dependencies {
         // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file for plugin from JetBrains Marketplace.
         plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
 
-        instrumentationTools()
         pluginVerifier()
         //For BasePlatformTestCase
         testFramework(TestFrameworkType.Platform)
@@ -87,7 +86,7 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
-            untilBuild = providers.gradleProperty("pluginUntilBuild")
+//            untilBuild = providers.gradleProperty("pluginUntilBuild")
         }
     }
 
